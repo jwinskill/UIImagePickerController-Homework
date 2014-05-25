1d941f2908c16b7c3d7efbed72ba1276ab3277ab
Josh Winskill <jwinskill@gmail.com>
2014-05-24 17:00:53 -0700

    Initial Commit

---
+++ Homework2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Homework2
+//
+//  Created by Josh Winskill on 5/24/14.
+//  Copyright (c) 2014 Josh Winskill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Homework2Tests/Homework2Tests.m
@@ -0,0 +1,34 @@
+//
+//  Homework2Tests.m
+//  Homework2Tests
+//
+//  Created by Josh Winskill on 5/24/14.
+//  Copyright (c) 2014 Josh Winskill. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Homework2Tests : XCTestCase
+
+@end
+
+@implementation Homework2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
