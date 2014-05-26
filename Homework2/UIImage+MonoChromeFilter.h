0ead8a5a649ff7cdbafd343cf442ec600931f9b6
Josh Winskill <jwinskill@gmail.com>
2014-05-26 14:14:44 -0700

    first commit

---
+++ Homework2/UIImage+MonoChromeFilter.h
@@ -0,0 +1,13 @@
+//
+//  UIImage+MonoChromeFilter.h
+//  Extending Class Functionality
+//
+//  Created by Josh Winskill on 5/25/14.
+//  Copyright (c) 2014 Josh Winskill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (MonoChromeFilter)
+
+@end
---
+++ Homework2/UIImage+MonoChromeFilter.m
@@ -0,0 +1,13 @@
+//
+//  UIImage+MonoChromeFilter.m
+//  Extending Class Functionality
+//
+//  Created by Josh Winskill on 5/25/14.
+//  Copyright (c) 2014 Josh Winskill. All rights reserved.
+//
+
+#import "UIImage+MonoChromeFilter.h"
+
+@implementation UIImage (MonoChromeFilter)
+
+@end
